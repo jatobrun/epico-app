3aac648c139b0ad202de120b16f5d97b223add19
Ronny <58896049+rjoza@users.noreply.github.com>
2022-04-02 15:43:29 -0500

    [ADD] "Agregar Calendario"

--- src/screens/Calendar.tsx
+++ src/screens/Calendar.tsx
@@ -1,10 +1,31 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Button, DatePicker, Icon, Modal,Provider } from '@ant-design/react-native';
 
 export default function Calendar() {
   return (
-    <View>
-      <Text>Page Calendar</Text>
-    </View>
+    
+    <Provider>
+    <View >
+    
+   
+    
+  <Text>Calendario</Text>
+  <DatePicker 
+              mode="date"
+              defaultDate={new Date()}
+              minDate={new Date(2015, 7, 6)}
+              maxDate={new Date(2026, 11, 3)}
+            
+              format="YYYY-MM-DD"/>
+   
+
+
+   
+
+</View>
+
+</Provider>  
+
   )
 }
