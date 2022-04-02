283b90e86b9bac9047e900b7f31265df947de112
Victor Pantoja <vpan1967@gmail.com>
2022-04-01 22:04:46 -0500

    Pantalla Login V1

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View } from 'react-native';
+import React from 'react';
+import DisplayImages from '../Components/DisplayImages';
+import CustomImput from '../Components/CustomImput';
+import CustomButton from '../Components/CustomButton';
 
 export default function Login() {
   return (
     <View>
-      <Text>Login</Text>
+      <DisplayImages/>
+      <CustomImput/>
+      <CustomButton text='Iniciar Sesion' tipo='PRIMARY'/>
+      <CustomButton text='Olvidó su Contraseña ?' tipo='INFO'/>
+      <CustomButton text='No tiene una cuenta?, registrese aquí' tipo='INFO'/>
     </View>
   )
 }
+
