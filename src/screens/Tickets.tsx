03df86f8473ffea2826b096858b6825380be7519
Ronny <58896049+rjoza@users.noreply.github.com>
2022-04-05 00:11:49 -0500

    [ADD]"Boton creaer y ver ticket" pantalla Tickets

--- src/screens/Tickets.tsx
+++ src/screens/Tickets.tsx
@@ -1,43 +1,117 @@
-import { View, Text } from 'react-native'
+import { View, Text,Alert,Modal,StyleSheet,Pressable } from 'react-native'
 import Maticon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-import React from 'react'
-import { Button, DatePicker, Icon, Modal,WhiteSpace, WingBlank } from '@ant-design/react-native';
+import React ,{ useState } from 'react';
+import { Button, DatePicker, Icon,WhiteSpace, WingBlank } from '@ant-design/react-native';
+import { TextInput } from 'react-native-gesture-handler';
 
 export default function Tickets() {
    let iconName = 'InfoCircleTwoTone';
+   const [modalVisible, setModalVisible] = useState(false);
+   const [text,onChangeText]=React.useState("Texto aqui");
   return (
    
    
-      <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-      <WingBlank>
+    <View style={styles.centeredView}>
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={modalVisible}
+      onRequestClose={() => {
+        Alert.alert("Modal has been closed.");
+        setModalVisible(!modalVisible);
+      }}
+    >
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+       
+      
       
       <Maticon name={iconName} size={48} color='#FFF' />
         <Text>Cita creada exitosamente</Text>
         <Text>AgrÃ©galo a tu calendario</Text>
-  
-      <WingBlank
-        style={{
-          marginTop: 10,
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}
-      >
-    
-        <Button type="ghost">Google Calendar</Button>
-       
-        <Button type="ghost" >
-          Exportar .Cal
-        </Button>
-      
         
-      </WingBlank>
-      <WhiteSpace />
-      <Button type="primary">Entendido</Button>
-        <WhiteSpace />
-    </WingBlank>
-    </View>
-  
-  )
+          <Pressable
+            style={[styles.button, styles.buttonClose]}
+            onPress={() => setModalVisible(!modalVisible)}
+          >
+          <Text style={styles.textStyle}>Entendido</Text>
+
+          </Pressable>
+        </View>
+      </View>
+    </Modal>
+    <Pressable
+      style={[styles.button, styles.buttonOpen]}
+      onPress={() => setModalVisible(true)}
+    >
+     
+      <Text style={styles.textStyle}>Crear Ticket</Text>
+      
+    </Pressable>
+    <Pressable
+     style={[styles.button, styles.buttonOpen]}
+     onPress={() => setModalVisible(true)}>
+      <Text style={styles.textStyle}>Ver Tickets</Text>
+    </Pressable>
+  </View>
+);
+};
+
+const styles = StyleSheet.create({
+
+ 
+centeredView: {
+  flex: 1,
+  justifyContent: "center",
+  alignItems: "center",
+  marginTop: 22
+},
+
+input: {
+  height:40,
+  margin:12,
+  borderWidth:1,
+  padding:10
+
+},
+modalView: {
+  margin: 20,
+  backgroundColor: "white",
+  borderRadius: 20,
+  padding: 35,
+  alignItems: "center",
+  shadowColor: "#000",
+  shadowOffset: {
+    width: 0,
+    height: 2
+  },
+  shadowOpacity: 0.25,
+  shadowRadius: 4,
+  elevation: 5
+},
+button: {
+  borderRadius: 20,
+  padding: 10,
+  elevation: 2,
+  marginBottom:10
+},
+buttonOpen: {
+  backgroundColor: "#4680ff",
+},
+buttonClose: {
+  backgroundColor: "#4680ff",
+},
+textStyle: {
+  color: "white",
+  fontWeight: "bold",
+  textAlign: "center",
+  fontSize:25
+
+},
+modalText: {
+  marginBottom: 15,
+  textAlign: "center"
 }
+  
+});
