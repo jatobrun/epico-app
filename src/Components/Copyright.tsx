334dc9e8d6dfe9c222e2370c32db9e705c53955e
Santiago Mora <santiaguchis@hotmail.com>
2022-04-17 14:15:57 -0500

    Fix Refactor Derechos to MyCopiright

--- src/Components/Copyright.tsx
+++ src/Components/Copyright.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { View, Text } from 'react-native';
 import { colors } from '../theme/colors';
 
-export const Derechos = () => {
+export const MyCopyright = () => {
   return (
     <View>
         <Text style={{ 
