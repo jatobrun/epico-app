dfd4133407d51594fc8fcbb6ea1ba30698fae165
Santiago Mora <santiaguchis@hotmail.com>
2022-04-21 21:33:34 -0500

    [FIX] Resolve screens

--- src/features/activity/screens/MyActivitiesScreen.tsx
+++ src/features/activity/screens/MyActivitiesScreen.tsx
@@ -1,13 +1,43 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { Button, Text } from 'react-native-paper';
 import { PageTitle } from '../../../components/PageTitle';
 
 export const MyActivitiesScreen = () => {
   return (
-    <View>
+    <>
       <PageTitle title="Mis actividades" />
-      <Text>My Activities Screen</Text>
-    </View>
+      <View style={ styles.container }>
+        <View style={styles.button}>
+          <Text style={styles.text}>Bienvenida</Text>
+          <Button mode="contained" onPress={() => {}}>Empezar</Button>
+        </View>
+
+        <View style={styles.button}>
+          <Text style={styles.text}>Tour ÉPICO (opcional)</Text>
+          <Button mode="contained" onPress={() => {}} >En proceso</Button>
+        </View>
+
+        <View style={styles.button}>
+          <Text style={styles.text}>Actividades del Programa Introductorio</Text>
+          <Button mode="contained" onPress={() => {}}>En proceso</Button>
+        </View>
+      </View>
+    </>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 0.5,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  text: { fontSize: 15 },
+  button:{
+    borderRadius:30,
+    width:'70%',
+    paddingBottom: 50
+  },
+});
